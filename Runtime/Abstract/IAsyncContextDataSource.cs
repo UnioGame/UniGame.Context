1b309c863c732dc355dbb89bda77941a192d3311
Mefodei <mefodei.link@gmail.com>
2020-10-05 01:41:17 +0300

    - initialize

---
+++ Runtime/Abstract/IAsyncContextDataSource.cs
@@ -0,0 +1,10 @@
+﻿namespace UniGreenModules.UniContextData.Runtime.Interfaces
+{
+    using Cysharp.Threading.Tasks;
+    using UniCore.Runtime.Interfaces;
+
+    public interface IAsyncContextDataSource
+    {
+        UniTask<IContext> RegisterAsync(IContext context);
+    }
+}
