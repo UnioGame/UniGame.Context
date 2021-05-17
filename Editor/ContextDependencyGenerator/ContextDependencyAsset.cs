7e155a9483f289954499c93709d79bbf6ca85915
Mefodei <mefodei.link@gmail.com>
2021-05-17 03:29:10 +0300

    - update context dependency asset

---
+++ Editor/ContextDependencyGenerator/ContextDependencyAsset.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UniModules.UniGame.Context.Runtime.Context;
+using UniModules.UniGame.Core.Runtime.Interfaces;
+using UniModules.UniGame.Core.Runtime.SerializableType;
+using UnityEngine;
+
+namespace UniModules.UniGame.CoreModules.UniGame.Context.Editor.ContextDependencyGenerator
+{
+    [CreateAssetMenu(menuName = "UniGame/Context/DependencyAsset",fileName = nameof(ContextDependencyAsset))]
+    public class ContextDependencyAsset : ScriptableObject
+    {
+
+        public List<SType> generatorTypes = new List<SType>()
+        {
+            typeof(SomeDemoClass)
+        };
+
+
+#if ODIN_INSPECTOR
+        [Sirenix.OdinInspector.Button]
+#endif
+        public void Create()
+        {
+            var context = new EntityContext();
+            Type type = generatorTypes.FirstOrDefault();
+            if (type == null || type.IsAbstract || type.IsInterface) return;
+            
+            ProceedType(type,context);
+        }
+
+        private void ProceedType(Type type, IContext context)
+        {
+            var constructors = type
+                .GetConstructors()
+                .Where(x => x.IsPublic);
+            
+            Debug.Log($"Proceed Type {type.Name}");
+            
+            foreach (var constructor in constructors)
+            {
+                var parameters = constructor.GetParameters();
+                foreach (var parameterInfo in parameters)
+                {
+                    Debug.Log($"parameter : {parameterInfo.ParameterType.Name}");
+                }
+            }
+        }
+        
+    }
+    
+    [Serializable]
+    public class SomeDemoClass
+    {
+        public SomeDemoClass(List<int> values1,string value2)
+        {
+            
+        }
+    }
+    
+}
