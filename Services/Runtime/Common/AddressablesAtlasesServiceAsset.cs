c9af770ab9046de97d4e1edad22fbabe31b3584f
MikhailovSergei <mikhailovs@taktikagames.ru>
2021-07-01 16:15:41 +0300

    - update addressable atlas service

--- Services/Runtime/Common/AddressablesAtlasesServiceAsset.cs
+++ Services/Runtime/Common/AddressablesAtlasesServiceAsset.cs
@@ -1,27 +1,29 @@
-﻿using UnityEngine;
-
-namespace UniModules.UniGameFlow.GameFlow.Runtime.Services.Common
+﻿namespace UniModules.UniGameFlow.GameFlow.Runtime.Services.Common
 {
-    using Systems;
+    using UniModules.UniGame.AddressableTools.Runtime.Attributes;
+    using UniModules.UniGame.AddressableTools.Runtime.Extensions;
+    using UnityEngine;
+    using UnityEngine.AddressableAssets;
     using Cysharp.Threading.Tasks;
     using UniGame.AddressableTools.Runtime.SpriteAtlases;
     using UniGame.AddressableTools.Runtime.SpriteAtlases.Abstract;
     using UniGame.Core.Runtime.Interfaces;
 
-
     [CreateAssetMenu(menuName = "UniGame/GameSystem/Services/AddressablesAtlasesService",fileName = nameof(AddressablesAtlasesService))]
     public class AddressablesAtlasesServiceAsset : ServiceDataSourceAsset<IAddressablesAtlasesService>
     {
-#if ODIN_INSPECTOR
-        [Sirenix.OdinInspector.InlineEditor()]
-#endif
+        [DrawAssetReference]
         [SerializeField]
-        private AddressableSpriteAtlasConfiguration _configuration;
+        private AssetReferenceT<AddressableSpriteAtlasConfiguration> _configuration;
         
         protected override async UniTask<IAddressablesAtlasesService> CreateServiceInternalAsync(IContext context)
         {
-            var service = new AddressablesAtlasesService(_configuration);
+            var config = await _configuration.LoadAssetTaskAsync(context.LifeTime);
+            var service = new AddressablesAtlasesService(config);
+            
             context.Publish<IAddressablesAtlasesLoader>(service);
+            context.Publish<IAddressableSpriteAtlasHandler>(config);
+            
             return service;
         }
     }
