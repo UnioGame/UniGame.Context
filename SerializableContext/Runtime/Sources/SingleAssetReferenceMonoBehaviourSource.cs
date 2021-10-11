ee0df50c28507c43158f6cea4e23c8e035892fd9
Mefodei <mefodei.link@gmail.com>
2021-10-11 23:59:53 +0300

    - remove missing

--- SerializableContext/Runtime/Sources/SingleAssetReferenceMonoBehaviourSource.cs
+++ SerializableContext/Runtime/Sources/SingleAssetReferenceMonoBehaviourSource.cs
@@ -3,15 +3,16 @@ using UniModules.UniGame.Core.Runtime.Extension;
 
 namespace UniModules.UniGame.SerializableContext.Runtime.Components
 {
+    using System;
     using Context.Runtime.Abstract;
     using Core.Runtime.Interfaces;
     using Cysharp.Threading.Tasks;
-    using UniCore.Runtime.ProfilerTools;
     using UniModules.UniGame.AddressableTools.Runtime.Extensions;
     
     using UnityEngine;
     using UnityEngine.AddressableAssets;
 
+    [Serializable]
     public class SingleAssetReferenceMonoBehaviourSource<TObject, TApi> : AsyncContextDataSource
         where TObject : MonoBehaviour, TApi
     {
@@ -28,7 +29,8 @@ namespace UniModules.UniGame.SerializableContext.Runtime.Components
 
             context.LifeTime.AddCleanUpAction(() => GameLog.Log($"Context {context.GetType().Name} {GetType().Name} LIFETIME FINISHED",Color.red));
             
-            var asset = await _prefabReference.LoadGameObjectAssetTaskAsync<TObject>(context.LifeTime)
+            var asset = await _prefabReference
+                .LoadGameObjectAssetTaskAsync<TObject>(context.LifeTime)
                 .AttachExternalCancellation(LifeTime.TokenSource);
             
             lock (this) {
@@ -41,6 +43,7 @@ namespace UniModules.UniGame.SerializableContext.Runtime.Components
                     _instance.gameObject.DestroyWith(LifeTime);
                 }
             }
+            
             if (!_instance) {
                 GameLog.LogError($"{GetType().Name} NULL TObject {nameof(TObject)} load from {_prefabReference}");
                 return context;
@@ -56,15 +59,9 @@ namespace UniModules.UniGame.SerializableContext.Runtime.Components
             return context;
         }
 
-        protected virtual async UniTask<TApi> OnInstanceReceive(TObject asset,IContext context)
-        {
-            return asset;
-        }
+        protected virtual UniTask<TApi> OnInstanceReceive(TObject asset,IContext context) => UniTask.FromResult<TApi>(asset);
         
-        protected virtual void OnInstanceRegistered(TObject asset, IContext context)
-        {
-            
-        }
+        protected virtual void OnInstanceRegistered(TObject asset, IContext context) { }
 
     }
 }
