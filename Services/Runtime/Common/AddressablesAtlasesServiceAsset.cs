09f8802d974db7ec39a3f9c171a583870b7b7c6a
Mefodei <mefodei.link@gmail.com>
2021-10-16 22:07:16 +0300

    - update altas service api

--- Services/Runtime/Common/AddressablesAtlasesServiceAsset.cs
+++ Services/Runtime/Common/AddressablesAtlasesServiceAsset.cs
@@ -6,34 +6,30 @@
     using UnityEngine.AddressableAssets;
     using Cysharp.Threading.Tasks;
     using UniGame.AddressableTools.Runtime.SpriteAtlases;
-    using UniGame.AddressableTools.Runtime.SpriteAtlases.Abstract;
     using UniGame.Core.Runtime.Interfaces;
 
-    [CreateAssetMenu(menuName = "UniGame/GameSystem/AddressablesAtlasesService",fileName = nameof(AddressablesAtlasesService))]
-    public class AddressablesAtlasesServiceAsset : ServiceDataSourceAsset<IAddressablesAtlasesService>
+    [CreateAssetMenu(menuName = "UniGame/GameSystem/AddressablesAtlasesService",fileName = "AddressablesAtlasesServiceSource")]
+    public class AddressablesAtlasesServiceAsset : ServiceDataSourceAsset<IAddressableAtlasService>
     {
 #if ODIN_INSPECTOR
         [Sirenix.OdinInspector.DrawWithUnity]
 #endif
         public AssetReferenceT<AddressableSpriteAtlasConfiguration> configuration;
 
-        public bool unloadConfigurationOnReset = false;
+        public bool unloadConfigurationOnReset = true;
         
-        protected override async UniTask<IAddressablesAtlasesService> CreateServiceInternalAsync(IContext context)
+        protected override async UniTask<IAddressableAtlasService> CreateServiceInternalAsync(IContext context)
         {
             var config = await configuration.LoadAssetTaskAsync(LifeTime);
-            await config.AddTo(LifeTime).Execute();
+            config.Initialize();
+            config.AddTo(LifeTime);
+
+            var service = config.AtlasService;
+            if (!unloadConfigurationOnReset) return service;
             
-            var service = new AddressablesAtlasesService(config);
-            if (unloadConfigurationOnReset)
-            {
-                config.AddTo(LifeTime);
-                service.AddTo(LifeTime);
-            }
-            
-            context.Publish<IAddressablesAtlasesLoader>(service);
-            context.Publish<IAddressableSpriteAtlasHandler>(config);
-            
+            config.AddTo(LifeTime);
+            service.AddTo(LifeTime);
+
             return service;
         }
     }
