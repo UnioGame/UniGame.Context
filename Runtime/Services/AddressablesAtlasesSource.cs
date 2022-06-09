78d2d696e156846d2f86d92319a6a0e4fe73ab63
Mefodei <mefodei.link@gmail.com>
2022-06-09 19:45:44 +0300

    move rx to module

---
+++ Runtime/Services/AddressablesAtlasesSource.cs
@@ -0,0 +1,34 @@
+﻿namespace UniModules.UniGameFlow.GameFlow.Runtime.Services.Common
+{
+    using UniModules.UniGame.AddressableTools.Runtime.Extensions;
+    using UniModules.UniGame.SerializableContext.Runtime.Addressables;
+    using UniModules.UniGame.AddressableTools.Runtime.SpriteAtlases;
+    using UniModules.UniGame.AddressableTools.Runtime.SpriteAtlases.Abstract;
+    using UniModules.UniCore.Runtime.Rx.Extensions;
+    using UnityEngine;
+    using UnityEngine.AddressableAssets;
+    using Cysharp.Threading.Tasks;
+    using UniGame.Core.Runtime.Interfaces;
+
+    [CreateAssetMenu(menuName = "UniGame/GameSystem/" + nameof(AddressablesAtlasesSource),fileName = nameof(AddressablesAtlasesSource))]
+    public class AddressablesAtlasesSource : ServiceDataSourceAsset<IAddressableAtlasService>
+    {
+#if ODIN_INSPECTOR
+        [Sirenix.OdinInspector.DrawWithUnity]
+#endif
+        public AssetReferenceT<AddressableSpriteAtlasAsset> configuration;
+
+        protected override async UniTask<IAddressableAtlasService> CreateServiceInternalAsync(IContext context)
+        {
+            var config = await configuration.LoadAssetTaskAsync(LifeTime);
+            config.Initialize();
+            config.AddTo(LifeTime);
+
+            var service = AddressableSpriteAtlasAsset.AtlasService;
+            
+            config.AddTo(LifeTime);
+
+            return service;
+        }
+    }
+}
---
+++ Runtime/Services/AssetReferenceServiceAsset.cs
@@ -0,0 +1,15 @@
+﻿namespace UniModules.UniGameFlow.GameFlow.Runtime.Systems
+{
+    using System;
+    using Interfaces;
+    using Services;
+    using UnityEngine.AddressableAssets;
+
+    [Serializable]
+    public class AssetReferenceServiceAsset : AssetReferenceT<ServiceDataSourceAsset<IGameService>>
+    {
+        public AssetReferenceServiceAsset(string guid) : base(guid)
+        {
+        }
+    }
+}
